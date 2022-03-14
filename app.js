04dfd178dc4a317b6574045957fbc8d3dba0fd93
Hüseyin Bilen <huseyin.bilen@outlook.com.tr>
2022-03-15 02:03:26 +0300

    make it better

--- app.js
+++ app.js
@@ -1,27 +1,69 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+
+const pageRoute = require('./routes/pageRoutes');
+const courseRoute = require('./routes/courseRoutes');
+const categoryRoute = require('./routes/categoryRoutes');
+const userRoute = require('./routes/userRoutes');
 
 const app = express();
+
+// Connect DB
+mongoose
+  .connect('mongodb+srv://huseyinbilen:idjum0084xihYkBK@cluster0.iuzbh.mongodb.net/smartedu-db?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('DB Connected Successfully.');
+  });
 
 // Template Engine
 app.set('view engine', 'ejs');
 
+// Global Variable
+global.userIN = null;
+
 // Middlewares
 app.use(express.static('public'));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 // Routes
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: 'index'
-  });
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
 });
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: 'about'
-  });
-});
-
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App Started on port ${port}`);
 });
